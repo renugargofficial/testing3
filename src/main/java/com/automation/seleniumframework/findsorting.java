05e30260fc873016c4eb04877e5d4f736bc20623
test jenkins <renugrg581@gmail.com>
2026-01-12 20:55:26 +0530

    new class has been added

---
+++ src/main/java/com/automation/seleniumframework/findsorting.java
@@ -0,0 +1,12 @@
+package com.automation.seleniumframework;
+
+
+
+public class findsorting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("soting ytersted");
+	}
+
+}
